8cc6e7832dd86eb0adcdb1574a9a4ca91a2793de
pasly0920 <86864534+pasly0920@users.noreply.github.com>
2021-09-27 12:08:02 +0900

    Delete Momentum directory

--- Momentum/JS/setup.js
+++
@@ -1,9 +0,0 @@
-const setup = document.querySelector("#setup");
-const setuptext = document.querySelector("#setup h2");
-const btn_reset = document.querySelector("#setup a#reset");
-
-btn_reset.addEventListener("click", ()=>{
-    console.log("btn_clicked");
-    localStorage.clear();
-    window.location.reload();//refresh;
-});
